d3b5b7b2a5682dd2a1e70aebf9aba2e56c888be1
junyan-k <junyan_k@u.nus.edu>
2025-08-20 16:37:48 +0800

    Allow Uxie to repeat commands
    Uxie now repeats commands typed in. Framework has been provided
    to add more command logic in future. Inputting "goodbye" or
    "bye" will exit the program.
    
    Also added some personality I KNOW this isn't supposed to be in
    one commit okay i'm trying my best and git says its all in one
    hunk and i dont know how to separate it okay

--- src/main/java/Uxie.java
+++ src/main/java/Uxie.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 public class Uxie {
+    // Uxie's dialogue is indented with 4 whitespace
+
     // used to space messages
     private final static String LINE_BREAK =
-            "____________________________________________________________";
+            "    ____________________________________________________________";
 
     public static void main(String[] args) {
         // Output: Welcome (could avoid hardcoding these greetings in future)
         System.out.println(LINE_BREAK);
-        System.out.println("Hello! I'm Uxie!");
-        System.out.println("What can I do for you?");
+        System.out.println("    Oh. Hello there, Trainer.");
+        System.out.println("    Uxie uxie, or whatever.");
+        System.out.println("    Dare I ask why you have summoned me?");
         System.out.println(LINE_BREAK);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        // Receive commands
+        Scanner s = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            String userCommand = s.nextLine(); // get next command
+            System.out.println(LINE_BREAK);
+            switch (userCommand) {
+                case "goodbye":
+                case "bye":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("    " + userCommand); // echo command
+                    System.out.println(LINE_BREAK);
+            }
+        }
+
+        // Goodbye
+        System.out.println("    That is all? Very well. Goodbye.");
         System.out.println(LINE_BREAK);
     }
 }
