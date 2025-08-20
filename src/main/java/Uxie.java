5acfa297ae7349b7843de98068b2489be8bf710f
junyan-k <junyan_k@u.nus.edu>
2025-08-20 16:21:21 +0800

    Rename to Uxie (L0)

---
+++ src/main/java/Uxie.java
@@ -0,0 +1,15 @@
+public class Uxie {
+    // used to space messages
+    private final static String LINE_BREAK =
+            "____________________________________________________________";
+
+    public static void main(String[] args) {
+        // Output: Welcome (could avoid hardcoding these greetings in future)
+        System.out.println(LINE_BREAK);
+        System.out.println("Hello! I'm Uxie!");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE_BREAK);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE_BREAK);
+    }
+}
