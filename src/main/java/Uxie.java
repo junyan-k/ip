2ba031abd593923e2e1fba9b606f2e6dfb452a3d
junyan-k <junyan_k@u.nus.edu>
2025-08-20 16:54:04 +0800

    Add list functionality

--- src/main/java/Uxie.java
+++ src/main/java/Uxie.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Uxie {
     // Uxie's dialogue is indented with 4 whitespace
 
     // used to space messages
     private final static String LINE_BREAK =
             "    ____________________________________________________________";
+    private final static ArrayList<String> listContents = new ArrayList<>();
 
     public static void main(String[] args) {
         // Output: Welcome (could avoid hardcoding these greetings in future)
@@ -21,12 +24,29 @@ public class Uxie {
             String userCommand = s.nextLine(); // get next command
             System.out.println(LINE_BREAK);
             switch (userCommand) {
+                case "list":
+                    // output contents of list
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 1; i <= listContents.size(); i++) {
+                        sb.append("    "); // there's probably a better way to do this
+                        sb.append(i);
+                        sb.append(". ");
+                        sb.append(listContents.get(i-1));
+                        if (i != listContents.size()) { sb.append("\n"); }
+                    }
+                    System.out.println(sb);
+                    System.out.println(LINE_BREAK);
+                    break;
+
                 case "goodbye":
                 case "bye":
                     running = false;
                     break;
+
                 default:
-                    System.out.println("    " + userCommand); // echo command
+                    // store command to list
+                    listContents.add(userCommand);
+                    System.out.println("    added " + userCommand); // echo command
                     System.out.println(LINE_BREAK);
             }
         }
