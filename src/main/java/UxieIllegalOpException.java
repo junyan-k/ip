825f3fc862460395e3c98ffb8170360dfda65f5c
junyan-k <junyan_k@u.nus.edu>
2025-08-22 10:46:16 +0800

    Add UxieIllegalOpException
    UxieIllegalOpException represents exceptions to do with illegal operations.

---
+++ src/main/java/UxieIllegalOpException.java
@@ -0,0 +1,21 @@
+/**
+ * Represents UxieExceptions to do with illegal operations.
+ */
+
+public class UxieIllegalOpException extends UxieException {
+
+    public UxieIllegalOpException(String message) {
+        super(message);
+    }
+
+    /**
+     * Returns exception as string in format:
+     * "Can't do that, trainer. <message>"
+     *
+     * @return formatted string
+     */
+    @Override
+    public String toString() {
+        return String.format("Can't do that, trainer. %s", this.getMessage());
+    }
+}
