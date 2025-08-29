506c66e58df6271567422c91f3a7e607e619ee19
junyan-k <junyan_k@u.nus.edu>
2025-08-29 14:32:30 +0800

    Format JavaDoc of exceptions package

--- src/main/java/uxie/exceptions/UxieSyntaxException.java
+++ src/main/java/uxie/exceptions/UxieSyntaxException.java
@@ -2,13 +2,17 @@ package uxie.exceptions;
 
 /**
  * Indicates an error in user input syntax.
- * Details are provided in exception message.
+ * Details are provided in message.
  *
  * @author junyan-k
  */
 
 public class UxieSyntaxException extends UxieException {
 
+    /**
+     * Generates UxieSyntaxException with provided message.
+     * @see uxie.exceptions.UxieException#UxieException(String)
+     */
     public UxieSyntaxException(String message) {
         super(message);
     }
