2c41aed2a0880c054c723f48a122b8e2c17ae8e2
junyan-k <junyan_k@u.nus.edu>
2025-09-05 12:41:11 +0800

    Add Gui classes

---
+++ src/main/java/uxie/interfaces/ui/GuiMain.java
@@ -0,0 +1,16 @@
+package uxie.interfaces.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main class of JavaFX GUI.
+ */
+public class GuiMain extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+    }
+
+}
