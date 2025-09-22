3028b32ec5e451733af277937517bccb58b23a86
junyan-k <junyan_k@u.nus.edu>
2025-09-22 15:05:42 +0800

    Set program window title to Uxie

--- src/main/java/uxie/interfaces/ui/GuiMain.java
+++ src/main/java/uxie/interfaces/ui/GuiMain.java
@@ -45,6 +45,7 @@ public class GuiMain extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Uxie");
             fxmlLoader.<MainWindow>getController().setUxie(uxie);
             stage.show();
         } catch (IOException e) {
